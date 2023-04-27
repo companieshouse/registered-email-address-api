0b0d6593ae5fdece7629efdee3fa2421fa666b7f
carolineward2 <cward1@companieshouse.gov.uk>
2023-04-27 13:42:30 +0100

    testing springdoc-openapi-ui

--- src/main/java/uk/gov/companieshouse/registeredemailaddressapi/controller/RegisteredEmailAddressController.java
+++ src/main/java/uk/gov/companieshouse/registeredemailaddressapi/controller/RegisteredEmailAddressController.java
@@ -1,17 +1,21 @@
 package uk.gov.companieshouse.registeredemailaddressapi.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegisteredEmailAddressController {
 
     @GetMapping("/registered-email-address/healthcheck")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> getHealthCheck() {
         return ResponseEntity.ok().body("Registered Email Address Service is Healthy");
 
     }
+
 
 }
