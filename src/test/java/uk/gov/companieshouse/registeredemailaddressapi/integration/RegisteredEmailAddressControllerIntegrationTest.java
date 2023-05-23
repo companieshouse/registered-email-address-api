03d34c4442d8d1096271a72a19e5f3d8837dafd2
carolineward2 <cward1@companieshouse.gov.uk>
2023-05-23 14:11:18 +0100

    feature/ECCT-284-Put-rea-endpoint - fix integartion tests running with mvn command

--- src/test/java/uk/gov/companieshouse/registeredemailaddressapi/integration/RegisteredEmailAddressControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/registeredemailaddressapi/integration/RegisteredEmailAddressControllerIntegrationTest.java
@@ -1,6 +1,6 @@
 package uk.gov.companieshouse.registeredemailaddressapi.integration;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
