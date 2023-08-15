772f57849585d8f2fa3cbea5aa6e0e0040ea14b5
carolineward2 <127123837+carolineward2@users.noreply.github.com>
2023-08-15 12:18:02 +0100

    fix-formatting-on-validation-error (#34)
    * fix-formatting-on-validation-error

--- src/main/java/uk/gov/companieshouse/registeredemailaddressapi/service/ValidationService.java
+++ src/main/java/uk/gov/companieshouse/registeredemailaddressapi/service/ValidationService.java
@@ -1,12 +1,12 @@
 package uk.gov.companieshouse.registeredemailaddressapi.service;
 
-import com.google.gson.GsonBuilder;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.model.validationstatus.ValidationStatusError;
 import uk.gov.companieshouse.api.model.validationstatus.ValidationStatusResponse;
 import uk.gov.companieshouse.registeredemailaddressapi.model.dao.RegisteredEmailAddressDAO;
 import uk.gov.companieshouse.registeredemailaddressapi.utils.ApiLogger;
-import uk.gov.companieshouse.service.rest.err.Errors;
+
+import java.util.ArrayList;
 
 import static uk.gov.companieshouse.registeredemailaddressapi.utils.ValidationUtils.*;
 
@@ -15,7 +15,7 @@ public class ValidationService {
 
     public ValidationStatusResponse validateRegisteredEmailAddress(RegisteredEmailAddressDAO registeredEmailAddress,
                                                                    String requestId) {
-        var errors = new Errors();
+        var errors = new ArrayList<ValidationStatusError>();
         if (isNotNull(registeredEmailAddress.getData(),
                 "registered_email_address",
                 errors,
@@ -39,34 +39,23 @@ public class ValidationService {
         return formatValidationStatusResponse(errors, registeredEmailAddress, requestId);
     }
 
-    private ValidationStatusResponse formatValidationStatusResponse(Errors validationErrors,
+    private ValidationStatusResponse formatValidationStatusResponse(ArrayList<ValidationStatusError> validationErrors,
                                                                     RegisteredEmailAddressDAO registeredEmailAddress,
                                                                     String requestId) {
         var validationStatus = new ValidationStatusResponse();
 
-        if (!validationErrors.hasErrors()) {
+        if (validationErrors.isEmpty()) {
             ApiLogger.infoContext(requestId, String.format("Validation Successful for TransactionId %s",
                     registeredEmailAddress.getTransactionId()));
-
             validationStatus.setValid(true);
         } else {
+
             ApiLogger.infoContext(requestId, String.format("Validation Unsuccessful for TransactionId %s, return errors",
                     registeredEmailAddress.getTransactionId()));
-            var errorsAsJsonString = convertErrorsToJsonString(validationErrors);
             validationStatus.setValid(false);
-            var errors = new ValidationStatusError[1];
-            var error = new ValidationStatusError();
-            error.setError(errorsAsJsonString);
-            errors[0] = error;
-
-            validationStatus.setValidationStatusError(errors);
+            ValidationStatusError[] validationStatusErrors = validationErrors.toArray(new ValidationStatusError[0]);
+            validationStatus.setValidationStatusError(validationStatusErrors);
         }
         return validationStatus;
     }
-
-    private static String convertErrorsToJsonString(Errors validationErrors) {
-        var gson = new GsonBuilder().create();
-        return gson.toJson(validationErrors);
-    }
-
 }
--- src/main/java/uk/gov/companieshouse/registeredemailaddressapi/utils/ValidationUtils.java
+++ src/main/java/uk/gov/companieshouse/registeredemailaddressapi/utils/ValidationUtils.java
@@ -1,9 +1,8 @@
 package uk.gov.companieshouse.registeredemailaddressapi.utils;
 
 import org.springframework.stereotype.Component;
-import uk.gov.companieshouse.service.rest.err.Err;
-import uk.gov.companieshouse.service.rest.err.Errors;
-
+import uk.gov.companieshouse.api.model.validationstatus.ValidationStatusError;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 @Component
@@ -15,34 +14,38 @@ public class ValidationUtils {
 
     private ValidationUtils() { }
 
-    public static boolean isNotNull(Object toTest, String qualifiedFieldName, Errors errs, String loggingContext) {
+    public static boolean isNotNull(Object toTest, String qualifiedFieldName, ArrayList<ValidationStatusError> errs, String loggingContext) {
         if (toTest == null) {
-            setErrorMsgToLocation(errs, qualifiedFieldName, NOT_NULL_ERROR_MESSAGE.replace("%s", qualifiedFieldName));
+            setErrorMsg(errs, qualifiedFieldName, NOT_NULL_ERROR_MESSAGE.replace("%s", qualifiedFieldName));
             ApiLogger.infoContext(loggingContext , qualifiedFieldName + " Field is null");
             return false;
         }
         return true;
     }
-    public static void isValidEmailAddress(String email, String qualifiedFieldName, Errors errs, String loggingContext) {
+    public static void isValidEmailAddress(String email, String qualifiedFieldName, ArrayList<ValidationStatusError> errs, String loggingContext) {
         var regex = "^.+@.+\\..+$";
         var pattern = Pattern.compile(regex);
         var matcher = pattern.matcher(email);
 
         if (!matcher.matches()) {
-            setErrorMsgToLocation(errs, qualifiedFieldName, String.format(INVALID_EMAIL_ERROR_MESSAGE, qualifiedFieldName));
+            setErrorMsg(errs, qualifiedFieldName, String.format(INVALID_EMAIL_ERROR_MESSAGE, qualifiedFieldName));
             ApiLogger.infoContext(loggingContext, "Email address is not in the correct format for " + qualifiedFieldName);
         }
     }
-    public static void isEmailAddressStatementAccepted(boolean acceptedEmailAddressStatement, String qualifiedFieldName, Errors errs, String loggingContext) {
+    public static void isEmailAddressStatementAccepted(boolean acceptedEmailAddressStatement, String qualifiedFieldName, ArrayList<ValidationStatusError> errs, String loggingContext) {
         if (!acceptedEmailAddressStatement) {
-            setErrorMsgToLocation(errs, qualifiedFieldName, String.format(ACCEPTED_EMAIL_ADDRESS_STATEMENT_ERROR_MESSAGE, qualifiedFieldName));
+            setErrorMsg(errs, qualifiedFieldName, String.format(ACCEPTED_EMAIL_ADDRESS_STATEMENT_ERROR_MESSAGE, qualifiedFieldName));
             ApiLogger.infoContext(loggingContext, "The " + qualifiedFieldName + " must be True.");
         }
     }
 
-    public static void setErrorMsgToLocation(Errors errors, String qualifiedFieldName, String msg){
-        final var error = Err.invalidBodyBuilderWithLocation(qualifiedFieldName).withError(msg).build();
-        errors.addError(error);
+    public static void setErrorMsg(ArrayList<ValidationStatusError> errs, String qualifiedFieldName, String msg){
+        ValidationStatusError validationStatusError = new ValidationStatusError();
+        validationStatusError.setError(msg);
+        validationStatusError.setLocation(qualifiedFieldName);
+        validationStatusError.setType("ch:validation");
+        validationStatusError.setLocationType("json-path");
+        errs.add(validationStatusError);
     }
 
 
