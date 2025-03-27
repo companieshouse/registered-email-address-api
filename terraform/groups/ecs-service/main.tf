ab682987b0d890e788d4fe78cadf7a63bbc5d63e
cryder-ch <cryder@companieshouse.gov.uk>
2025-03-27 16:05:18 +0000

    Updated ecs module

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.253"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.296"
   # Environmental configuration
   environment             = var.environment
   aws_region              = var.aws_region
